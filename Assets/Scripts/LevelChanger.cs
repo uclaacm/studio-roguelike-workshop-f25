32ad31e628ca635530de57722ccebaacc1fc7f60
pacg0 <66505623+pacg0@users.noreply.github.com>
2025-11-03 18:19:15 -0800

    Random Scene Changer

--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -3,14 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
+    private int numScenes;
+    private void Start()
+    {
+        numScenes = SceneManager.sceneCountInBuildSettings;
+    }
 
-    public int targetSceneIndex;
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(targetSceneIndex);
+            SceneManager.LoadScene(Random.Range(0, numScenes));
         }
     }
 }
